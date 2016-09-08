8c751ca26f4d2889fcd99b3281da9af047a1e7bf
Kelly Victoria <kellyvkenney@gmail.com>
2016-09-08 11:49:36 -0700

    #3 created tweet element function

--- default.js
+++ default.js
@@ -25,7 +25,7 @@ submitPost.addEventListener('click', function(){
   var commentBox = document.getElementById('comment');
   var commentText = commentBox.value;
   var newTweet = {'content':commentText, name:'Nova'};
-  tweets.unshift(newTweet); //no quotes . unshift adds to front push adds to end
+  tweets.unshift(newTweet);
   confirm("Are you sure you want to post this?")
   showTweets(tweets);
   var homeScreen = document.getElementById('home-screen')
@@ -34,85 +34,42 @@ submitPost.addEventListener('click', function(){
   newPost.classList.add('hide')
 })
 
-function showTweets(tweets){   //function name and then parameter name
-  var showMe = document.getElementById('showme');
-  showMe.innerHTML="";
+function showTweets(tweets){
+  var mainView = document.getElementById('main-view');
+  mainView.innerHTML="";
   for( var i=0; i<tweets.length; i++) {
     var tweet = tweets[i];
-    var paragraphone = document.createElement('p');
-    var paragraphtwo = document.createElement('p');
+    var tweetText = document.createElement('p');
     var linebreak = document.createElement('br');
-    paragraphone.textContent="@" + tweet.name + " : ";
-    paragraphtwo.textContent= tweet.content ;
-    showMe.appendChild(paragraphone);
-    showMe.appendChild(linebreak);
-    showMe.appendChild(paragraphtwo);
-    showMe.appendChild(linebreak);
 
-  showMe.classList.add("user");
-
-}
+    mainView.classList.add('user');
+    tweetText.textContent="@" + tweet.name + " : " + tweet.content;
+    mainView.appendChild(tweetText);
+    mainView.appendChild(linebreak);
+  }
 }
 
+function tweetElement(tweet) {
+  // <div class="tweet">
+  //   <p class="tweet-username">
+  //     @keepo :
+  //   </p>
+  //   <p class="tweet-text">
+  //    meow meow
+  //   </p>
+  // </div>
+  var element = document.createElement('div');
+  element.classList.add('tweet');
+  var userParagraph = document.createElement('p');
+  userParagraph.classList.add('tweet-username');
+  userParagraph.textContent = '@' + tweet.name;
+  var tweetContent = document.createElement('p');
+  tweetContent.classList.add('tweet-text');
+  tweetContent.textContent = tweet.content;
+  element.appendChild(userParagraph);
+  element.appendChild(tweetContent);
 
+  return element;
+}
 
 showTweets(tweets)
-
-
-//mdn adding classes
-
-
-
-
-// Issue 3 - Can view another User's Timeline
-
-var userButton = document.getElementById('');
-postButton.addEventListener('click', function(){
-  var homeScreen = document.getElementById('')
-  homeScreen.classList.add('hide')
-  var newPost = document.getElementById('')
-  newPost.classList.remove('hide')
-})
-
-<!--
-// JAVASCRIPT for issue 3
-|
-|
-|
-
-// establish variables
-// container for tweets, pulls tweets with only chosen username
-// function that hides previous home page content and pulls issue 3 content, 'swap view'
-// eventlisteners that listen for clicks on the username
-
-var followerTimeline = document.createElement("div");
-var profileBlock = document.createElement("")
-element.setAttribute()
-
-function swap(location, next) {
-var current = document.getElementById(location);
-var active = current.getElementsByClassName('active')[0];
-active.classList.remove('active');
-active.classList.add('hide');
-
-var next = document.getElementById(next);
-next.classList.remove('hide');
-next.classLIst.add('active');
-}
-
-var navigation = document.getElementById('navigation');
-
-navigation.addEventListener('click', function(theEvent) {
-var id = theEvent.target.getAttribute('data-id');
-swap('view', id);
-});
-
-
-//swap('view', 'followed'); -->
-
-
-
-
-//still need to add user profile icon to each tweets, also add links to each tweet object that will direct
-//user to chosen profile page (issue 3)
-// add user photo to navbar
